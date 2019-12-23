b7b8e8542b3b26a0e9af0c6dcefb213e13d86b62
kaelsilva <kaelsilvaaraujo@gmail.com>
2019-12-23 16:59:30 -0300

    Adicionando botões, header e estilos à página de index do funcionário. #11

--- LojaDeRoupas/App.js
+++ LojaDeRoupas/App.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { Component } from 'react';
+import React, { Component } from 'react';
+import {StyleSheet, Text, TextInput, View, TouchableOpacity} from 'react-native';
 
 export default class App extends Component {
   constructor(props){
@@ -13,14 +12,68 @@ export default class App extends Component {
   render(){
     return (
       <View>
-        <Text>Hello world!</Text>
+        <View style={styles.head}>
+          <Text style={styles.headText}>LOJA DE ROUPAS</Text>
+        </View>
+
+        <View style={styles.body}>
+
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Funcionários</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Fornecedores</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Clientes</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Produtos</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Vendas</Text>
+          </TouchableOpacity>
+
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  head: {
+    backgroundColor: '#da552f',
+    justifyContent: 'center',
+    height: '10%',
+  },
+  headText: {
+    color: '#FFFFFF',
+    alignSelf: 'center',
+  },
   body: {
-    backgroundColor: Colors.white,
+    backgroundColor: '#eeeeee',
+    height: '90%',
+    width: '95%',
+    alignSelf: 'center',
+    padding: 20,
+    justifyContent: 'center',
+  },
+  button: {
+    height: 42,
+    borderRadius: 5,
+    borderWidth: 2,
+    backgroundColor: 'transparent',
+    marginTop: 10,
+    borderColor: '#da552f',
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    color: '#da552f',
+    alignSelf: 'center',
   },
 });
