c58955ea38f98612fcb68ce4b54fcb11dd879e0c
kaelsilva <kaelsilvaaraujo@gmail.com>
2020-01-05 21:25:23 -0300

    Adicionando arquivo de configuração do banco de dados. #5

---
+++ backend/sqlnode/src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: 'mysql',
+    host: 'localhost',
+    username: 'marketowner',
+    password: 'Marketowner123!@#',
+    database: 'clothingmarket',
+    define: {
+        timestamps: true,
+        underscored: true,
+    }
+}
