98d5c7a0dfa5d845aeb85d6f02b454ea19ed7b39
kaelsilva <kaelsilvaaraujo@gmail.com>
2020-01-10 09:35:07 -0300

    Adicionando rotas de usuário e de seus endereços. #13

--- backend/sqlnode/src/routes.js
+++ backend/sqlnode/src/routes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const UserController = require('./controllers/UserController');
+const AddressController = require('./controllers/AddressController');
 
 const routes = express.Router();
 
 routes.post('/users', UserController.store);
+routes.get('/users', UserController.index);
+
+routes.post('/users/:user_id/addresses', AddressController.store);
+routes.get('/users/:user_id/addresses', AddressController.index);
 
 module.exports = routes;
