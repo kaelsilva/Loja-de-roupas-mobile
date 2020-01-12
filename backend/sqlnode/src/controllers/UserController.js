f48b30306e6c7415f31d508f5a4fd7fc1f842b9b
kaelsilva <kaelsilvaaraujo@gmail.com>
2020-01-12 10:59:34 -0300

    Adicionando funcionalidade de buscar apenas um usuário. #13

--- backend/sqlnode/src/controllers/UserController.js
+++ backend/sqlnode/src/controllers/UserController.js
@@ -7,6 +7,12 @@ module.exports = {
         return res.json(users);
     },
 
+    async show(req, res){
+        const user = await User.findByPk(req.params.id);
+
+        return res.json(user);
+    },
+
     async store(req, res){
         const { name, email, password } = req.body;
         const user = await User.create( { name, email, password });
--- backend/sqlnode/src/routes.js
+++ backend/sqlnode/src/routes.js
@@ -6,8 +6,10 @@ const routes = express.Router();
 
 routes.post('/users', UserController.store);
 routes.get('/users', UserController.index);
+routes.get('/users/:id', UserController.show);
 routes.delete('/users/:id', UserController.destroy);
 routes.put('/users/:id', UserController.update);
+
 
 routes.post('/users/:user_id/addresses', AddressController.store);
 routes.get('/users/:user_id/addresses', AddressController.index);
