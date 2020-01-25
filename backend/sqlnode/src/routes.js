dea862a0a230c96bd21da0ed775b3fef8655d645
kaelsilva <kaelsilvaaraujo@gmail.com>
2020-01-25 20:47:32 -0300

    Criando rotas para clientes. #4

--- backend/sqlnode/src/routes.js
+++ backend/sqlnode/src/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const UserController = require('./controllers/UserController');
 const AddressController = require('./controllers/AddressController');
+const CustomerController = require('./controllers/CustomerController');
 
 const routes = express.Router();
 
@@ -10,10 +11,15 @@ routes.get('/users/:id', UserController.show);
 routes.delete('/users/:id', UserController.destroy);
 routes.put('/users/:id', UserController.update);
 
-
 routes.post('/users/:user_id/addresses', AddressController.store);
 routes.get('/users/:user_id/addresses', AddressController.index);
 routes.delete('/users/:user_id/addresses/:id', AddressController.destroy);
 routes.put('/users/:user_id/addresses/:id', AddressController.update);
+
+routes.post('/customers', CustomerController.store);
+routes.get('/customers', CustomerController.index);
+routes.get('/customers/:id', CustomerController.show);
+routes.delete('/customers/:id', CustomerController.destroy);
+routes.put('/customers/:id', CustomerController.update);
 
 module.exports = routes;
