ef535ef1da40f298b0b12705eca3b64bbdd1a702
kaelsilva <kaelsilvaaraujo@gmail.com>
2020-04-27 22:24:28 -0300

    Deletando campo de id da classe de produto (pois a migration já trata do id). #7

--- backend/sqlnode/src/models/Product.js
+++ backend/sqlnode/src/models/Product.js
@@ -3,7 +3,6 @@ const { Model, DataTypes } = require('sequelize');
 class Product extends Model {
     static init(sequelize){
         super.init({
-            id: DataTypes.INTEGER,
             name: DataTypes.STRING,
             quantity: DataTypes.INTEGER,
         }, {
