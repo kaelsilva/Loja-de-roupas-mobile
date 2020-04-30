c038e3502194a0c09a0a43a706f3f483f2ea8bf0
kaelsilva <kaelsilvaaraujo@gmail.com>
2020-04-30 19:50:52 -0300

    Adicionando arquivo da tela de clientes. #6

---
+++ frontend/src/pages/CustomerScreen/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default function CustomerScreen(){
+  return (
+    <View>
+      <Text>Customer Screen</Text>
+    </View>
+  );
+}
