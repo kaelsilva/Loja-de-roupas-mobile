1f7224f80390d20a41ed4e2be0b283054f6a2887
kaelsilva <kaelsilvaaraujo@gmail.com>
2020-04-30 20:27:50 -0300

    Importando e utilizando rotas no arquivo principal da aplicação.

--- frontend/App.js
+++ frontend/App.js
@@ -1,36 +1,17 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
-import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import '@react-native-community/masked-view';
-import CustomerScreen from './src/pages/CustomerScreen';
+import Routes from './src/routes';
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
-
-function DetailsScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
-    </View>
-  );
-}
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
+      <Routes />
     </NavigationContainer>
   );
 }
