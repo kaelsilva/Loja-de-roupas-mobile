977a3d6ce97ea74d3c093f4db1cb2c310defa7ba
kaelsilva <kaelsilvaaraujo@gmail.com>
2020-04-29 21:18:21 -0300

    Adicionando arquivos iniciais gerados pelo react-native-cli.

--- frontend/App.js
+++ frontend/App.js
@@ -1,79 +1,114 @@
-import React, { Component } from 'react';
-import {StyleSheet, Text, TextInput, View, TouchableOpacity} from 'react-native';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
 
-export default class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      
-    };
-  }
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
 
-  render(){
-    return (
-      <View>
-        <View style={styles.head}>
-          <Text style={styles.headText}>LOJA DE ROUPAS</Text>
-        </View>
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
 
-        <View style={styles.body}>
-
-          <TouchableOpacity style={styles.button}>
-            <Text style={styles.buttonText}>Funcionários</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={styles.button}>
-            <Text style={styles.buttonText}>Fornecedores</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={styles.button}>
-            <Text style={styles.buttonText}>Clientes</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={styles.button}>
-            <Text style={styles.buttonText}>Produtos</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={styles.button}>
-            <Text style={styles.buttonText}>Vendas</Text>
-          </TouchableOpacity>
-
-        </View>
-      </View>
-    );
-  }
-}
+const App: () => React$Node = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          <Header />
+          {global.HermesInternal == null ? null : (
+            <View style={styles.engine}>
+              <Text style={styles.footer}>Engine: Hermes</Text>
+            </View>
+          )}
+          <View style={styles.body}>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Step One</Text>
+              <Text style={styles.sectionDescription}>
+                Edit <Text style={styles.highlight}>App.js</Text> to change this
+                screen and then come back to see your edits.
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>See Your Changes</Text>
+              <Text style={styles.sectionDescription}>
+                <ReloadInstructions />
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Debug</Text>
+              <Text style={styles.sectionDescription}>
+                <DebugInstructions />
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Learn More</Text>
+              <Text style={styles.sectionDescription}>
+                Read the docs to discover what to do next:
+              </Text>
+            </View>
+            <LearnMoreLinks />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
+};
 
 const styles = StyleSheet.create({
-  head: {
-    backgroundColor: '#da552f',
-    justifyContent: 'center',
-    height: '10%',
+  scrollView: {
+    backgroundColor: Colors.lighter,
   },
-  headText: {
-    color: '#FFFFFF',
-    alignSelf: 'center',
+  engine: {
+    position: 'absolute',
+    right: 0,
   },
   body: {
-    backgroundColor: '#eeeeee',
-    height: '90%',
-    width: '95%',
-    alignSelf: 'center',
-    padding: 20,
-    justifyContent: 'center',
+    backgroundColor: Colors.white,
   },
-  button: {
-    height: 42,
-    borderRadius: 5,
-    borderWidth: 2,
-    backgroundColor: 'transparent',
-    marginTop: 10,
-    borderColor: '#da552f',
-    display: 'flex',
-    justifyContent: 'center',
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
   },
-  buttonText: {
-    color: '#da552f',
-    alignSelf: 'center',
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: '#ae34ae',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
   },
 });
+
+export default App;
