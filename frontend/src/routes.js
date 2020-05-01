7557d106e77d3a52739443cd79f73328d9f296bc
kaelsilva <kaelsilvaaraujo@gmail.com>
2020-05-01 08:43:28 -0300

    Adicionando instância do createStackNavigator para navegação de páginas. #4

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { createStackNavigation } from '@react-navigation/stack';
+import { createStackNavigator } from '@react-navigation/stack';
+const Stack = createStackNavigator();
 
-import Customers from './pages/CustomersScreen';
+
 import Users from './pages/UsersScreen';
+import Customers from './pages/CustomersScreen';
 
 export default function Routes(){
   return(
     <Stack.Navigator>
-        <Stack.Screen name="Customers" component={Customers} />
         <Stack.Screen name="Users" component={Users} />
-      </Stack.Navigator>
+        <Stack.Screen name="Customers" component={Customers} />
+    </Stack.Navigator>
   );
 }
