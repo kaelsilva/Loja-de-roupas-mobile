ef7b4ef44a65dfc31aa3fc3bdf82cdac171bc5da
kaelsilva <kaelsilvaaraujo@gmail.com>
2020-05-01 12:41:56 -0300

    Passando atributos de estilo de títulos para as telas através das rotas. #18

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 const Stack = createStackNavigator();
 
-
 import Users from './pages/UsersScreen';
 import Customers from './pages/CustomersScreen';
+
+import userHeader from './components/User/TitleStyle';
+import customerHeader from './components/Customer/TitleStyle';
 
 export default function Routes(){
   return(
     <Stack.Navigator>
-        <Stack.Screen name="Users" component={Users} />
-        <Stack.Screen name="Customers" component={Customers} />
+        <Stack.Screen name="Users" component={Users} options={userHeader} />
+        <Stack.Screen name="Customers" component={Customers} options={customerHeader} />
     </Stack.Navigator>
   );
 }
