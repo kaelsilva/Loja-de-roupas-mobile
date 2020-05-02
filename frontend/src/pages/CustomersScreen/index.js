e556c6da6de1f6fe6326a2e8ae5a988e7a6b8bb2
kaelsilva <kaelsilvaaraujo@gmail.com>
2020-05-02 13:12:07 -0300

    Resgatando dados da api através da instância do axios e exibindo alguns dados resgatados na tela. #6

--- frontend/src/pages/CustomersScreen/index.js
+++ frontend/src/pages/CustomersScreen/index.js
@@ -1,15 +1,54 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
+import api from '../../services/api';
 
-export default function CustomersScreen({ navigation }){
-  function navigateToUsers(){
-    navigation.navigate('Users')
+export default class CustomersScreen extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      customers: [],
+      // id: '',
+      // cpf: '',
+      // name: '',
+      // email: '',
+      // birthday: '',
+      // createdAt: '',
+      // updatedAt: '',
+    }
   }
 
-  return (
-    <View>
-      <Text>Customers Screen</Text>
-      <Button title="Navegar para usuários" onPress={navigateToUsers} />
-    </View>
-  );
+  async componentDidMount(){
+    const response = await api.get('/customers');
+
+    const customers = response.data;
+
+    this.setState({ customers: customers });
+  }
+
+  // navigateToUsers(){
+  //   navigation.navigate('Users')
+  // }
+
+
+  render(){
+    const { customers } = this.state;
+
+    return (
+      <View>
+        {customers.map(customer => (
+            <View>
+              <Text>Nome do cliente: {customer.name}</Text>
+              <Text>ID: {customer.id}</Text>
+              <Text>CPF: {customer.cpf}</Text>
+              <Text>E-mail: {customer.email}</Text>
+              {/* <Text>Data de nascimento: {customer.birthday}</Text> */}
+            </View>
+        ))}
+      </View>
+      // <View>
+      //   <Text>Customers Screen</Text>
+      //   <Button title="Navegar para usuários" onPress={navigateToUsers} />
+      // </View>
+    )
+  };
 }
