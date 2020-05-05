afd0cfe4c5031502a8890783b3887c46f493ad2c
kaelsilva <kaelsilvaaraujo@gmail.com>
2020-05-05 15:33:13 -0300

    Realizando conexão do modelo de fornecedores com o banco. #8

--- backend/sqlnode/src/database/index.js
+++ backend/sqlnode/src/database/index.js
@@ -4,11 +4,13 @@ const dbConfig = require('../config/database');
 const User = require('../models/User');
 const Customer = require('../models/Customer');
 const Product = require('../models/Product');
+const Provider = require('../models/Provider');
 
 const connection = new Sequelize(dbConfig);
 
 User.init(connection);
 Customer.init(connection);
 Product.init(connection);
+Provider.init(connection);
 
 module.exports = connection;
