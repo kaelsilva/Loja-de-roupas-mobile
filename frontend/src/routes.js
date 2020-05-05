972f1bd5ea1ce9dc8a93e4cfad058274d71d1ad4
kaelsilva <kaelsilvaaraujo@gmail.com>
2020-05-05 10:09:31 -0300

    Adicionando rotas para produtos. #7

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -5,10 +5,12 @@ const Stack = createStackNavigator();
 import AdminIndex from './pages/AdminIndex';
 import Users from './pages/UsersScreen';
 import Customers from './pages/CustomersScreen';
+import Products from './pages/ProductsScreen';
 
 import AdminHeader from './components/Admin/TitleStyle';
 import customerHeader from './components/Customer/TitleStyle';
 import usersHeader from './components/Users/TitleStyle';
+import productsHeader from './components/Products/TitleStyle';
 
 export default function Routes(){
   return(
@@ -16,6 +18,7 @@ export default function Routes(){
         <Stack.Screen name="AdminIndex" component={AdminIndex} options={AdminHeader} />
         <Stack.Screen name="Customers" component={Customers} options={customerHeader} />
         <Stack.Screen name="Users" component={Users} options={usersHeader} />
+        <Stack.Screen name='Products' component={Products} options={productsHeader} />
     </Stack.Navigator>
   );
 }
