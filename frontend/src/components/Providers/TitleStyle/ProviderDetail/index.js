e67ceca3ecdef9c7ed61c2f7a7363db35fa2b5a1
kaelsilva <kaelsilvaaraujo@gmail.com>
2020-05-15 13:26:15 -0300

    Adicionando estilo de título da tela de edição de fornecedores. #8

---
+++ frontend/src/components/Providers/TitleStyle/ProviderDetail/index.js
@@ -0,0 +1,13 @@
+const titleStyle = {
+  title: 'Editar fornecedor',
+  headerStyle: {
+    backgroundColor: '#da552f',           
+  },
+  headerTintColor: '#fff',
+  headerTitleStyle: {
+    fontWeight: 'bold',
+  },
+  headerTitleAlign: 'center'
+}
+
+export default titleStyle;
