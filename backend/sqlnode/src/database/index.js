c976f25644dbcc28eecb8781b88ad97ae5638148
kaelsilva <kaelsilvaaraujo@gmail.com>
2020-05-16 10:34:01 -0300

    Adicionando conexão do modelo de venda ao banco. #10

--- backend/sqlnode/src/database/index.js
+++ backend/sqlnode/src/database/index.js
@@ -5,6 +5,7 @@ const User = require('../models/User');
 const Customer = require('../models/Customer');
 const Product = require('../models/Product');
 const Provider = require('../models/Provider');
+const Sale = require('../models/Sale');
 
 const connection = new Sequelize(dbConfig);
 
@@ -12,5 +13,6 @@ User.init(connection);
 Customer.init(connection);
 Product.init(connection);
 Provider.init(connection);
+Sale.init(connection);
 
 module.exports = connection;
