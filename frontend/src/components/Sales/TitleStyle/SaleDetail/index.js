ac9c253ee037fff2474c91dff0b6113c1ca7303c
kaelsilva <kaelsilvaaraujo@gmail.com>
2020-05-18 14:10:27 -0300

    Adicionando estilo de título de tela de edição de venda. #10

---
+++ frontend/src/components/Sales/TitleStyle/SaleDetail/index.js
@@ -0,0 +1,13 @@
+const titleStyle = {
+  title: 'Editar venda',
+  headerStyle: {
+    backgroundColor: '#da552f',           
+  },
+  headerTintColor: '#fff',
+  headerTitleStyle: {
+    fontWeight: 'bold',
+  },
+  headerTitleAlign: 'center'
+}
+
+export default titleStyle;
