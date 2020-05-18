bf6bf08330b8b018cb421485ea3cc83402965857
kaelsilva <kaelsilvaaraujo@gmail.com>
2020-05-18 14:11:24 -0300

    Adicionando rotas para tela de criação de venda e seu devido estilo de título. #10

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -12,6 +12,7 @@ import Sales from './pages/Sales/SalesScreen';
 import CustomerCreation from './pages/Customers/CustomerCreation';
 import ProductCreation from './pages/Products/ProductCreation';
 import ProviderCreation from './pages/Providers/ProviderCreation';
+import SaleCreation from './pages/Sales/SaleCreation';
 
 import adminHeader from './components/Admin/TitleStyle';
 import customerHeader from './components/Customer/TitleStyle/CustomersIndex';
@@ -24,6 +25,7 @@ import customerCreationHeader from './components/Customer/TitleStyle/CustomerCre
 import productCreationHeader from './components/Products/TitleStyle/ProductCreation';
 import providerDetailHeader from './components/Providers/TitleStyle/ProviderDetail';
 import salesHeader from './components/Sales/TitleStyle/SalesIndex';
+import saleCreationHeader from './components/Sales/TitleStyle/SaleCreation';
 
 import CustomerDetail from './pages/Customers/CustomerDetail';
 import ProductDetail from './pages/Products/ProductDetail';
@@ -44,6 +46,7 @@ export default function Routes(){
         <Stack.Screen name='ProviderDetail' component={ProviderDetail} options={providerDetailHeader}/>
         <Stack.Screen name='ProviderCreation' component={ProviderCreation} options={providerDetailHeader}/>
         <Stack.Screen name='Sales' component={Sales} options={salesHeader} />
+        <Stack.Screen name='SaleCreation' component={SaleCreation} options={saleCreationHeader} />
     </Stack.Navigator>
   );
 }
