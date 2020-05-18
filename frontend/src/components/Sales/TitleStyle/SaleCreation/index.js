5ab651ea69bcd49f55fbddbf2f9a72f652d994d1
kaelsilva <kaelsilvaaraujo@gmail.com>
2020-05-18 14:10:49 -0300

    Adicionando estilo de título de tela de criação de venda. #10

---
+++ frontend/src/components/Sales/TitleStyle/SaleCreation/index.js
@@ -0,0 +1,13 @@
+const titleStyle = {
+  title: 'Nova venda',
+  headerStyle: {
+    backgroundColor: '#da552f',           
+  },
+  headerTintColor: '#fff',
+  headerTitleStyle: {
+    fontWeight: 'bold',
+  },
+  headerTitleAlign: 'center'
+}
+
+export default titleStyle;
